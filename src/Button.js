bcc9eed365ad28202dd07faa193ed0910e2ad3b0
DongWon Sehr <dongwon@its-newid.com>
2022-08-06 14:54:39 +0900

    apply css by module.css

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,9 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({ text }) {
     return (
-        <button
-            style={{
-                backgroundColor: "tomato",
-                color: "white",
-            }}
-        >
+        <button className={styles.btn}>
             {text}
         </button>
     );
