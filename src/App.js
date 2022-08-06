225ca87a581aed27373605c672a0eea2edc49a0a
DongWon Sehr <dongwon@its-newid.com>
2022-08-06 15:07:49 +0900

    use useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((curr) => curr + 1);
+
+    console.log("call an API");
+
     return (
         <div>
             <h1 className={styles.title}>Welcome Back!</h1>
-            <Button text={"Continue"} />
+            <h1 className={styles.title}>{counter}</h1>
+            <Button onClick={onClick} text={"Continue"} />
         </div>
     );
 }
