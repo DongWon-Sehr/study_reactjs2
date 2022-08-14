1fdc375727926208ee9525e6f9726f0f7ca39922
DongWon Sehr <dongwon@its-newid.com>
2022-08-14 21:11:44 +0900

    + menubar components

---
+++ src/components/MenuBar.js
@@ -0,0 +1,17 @@
+function MenuBar() {
+    return (
+        <div id="menu-bar">
+            <a href="/">Home</a>
+            <span> | </span>
+            <a href="/app-default">App Default</a>
+            <span> | </span>
+            <a href="/app-todo">App Todo</a>
+            <span> | </span>
+            <a href="/app-coins">App Coins</a>
+            <span> | </span>
+            <a href="/app-movies">App Movies</a>
+        </div>
+    );
+}
+
+export default MenuBar;
