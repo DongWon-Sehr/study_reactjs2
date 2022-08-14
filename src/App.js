f83da527f46e0900dd2bbb9c24e26156cab97d69
DongWon Sehr <dongwon@its-newid.com>
2022-08-14 20:58:55 +0900

    + heading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 function App() {
     return (
-        <div id="menu-bar">
-            <a href="/">Home</a>
-            <span> | </span>
-            <a href="/app-default">App Default</a>
-            <span> | </span>
-            <a href="/app-todo">App Todo</a>
-            <span> | </span>
-            <a href="/app-coins">App Coins</a>
+        <div>
+            <div id="menu-bar">
+                <a href="/">Home</a>
+                <span> | </span>
+                <a href="/app-default">App Default</a>
+                <span> | </span>
+                <a href="/app-todo">App Todo</a>
+                <span> | </span>
+                <a href="/app-coins">App Coins</a>
+            </div>
+            <h1>Home</h1>
         </div>
     );
 }
