1e55075f2e75f8427a56f15bfd9869be8085a43f
DongWon Sehr <dongwon@its-newid.com>
2022-08-14 18:30:42 +0900

    load AppCoins

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import AppTodo from "./AppTodo";
+// import AppTodo from "./AppTodo";
+import AppCoins from "./AppCoins";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <App />
-    <AppTodo />
+    // <AppTodo />
+    <AppCoins />
 );
