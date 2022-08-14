e5764bed4ffbc5b49abdee2e984137a5c647fe31
DongWon Sehr <dongwon@its-newid.com>
2022-08-14 20:54:07 +0900

    change app to appDefault

---
+++ src/AppDefault.js
@@ -0,0 +1,72 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function ShowData() {
+    useEffect(() => {
+        console.log("hi :)");
+        return () => console.log("bye :(");
+    }, []);
+    return <h1>My Data</h1>
+}
+
+function AppDefault() {
+    const [showing, setShowing] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClickCount = () => setCounter((curr) => curr + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    const onClickShowing = () => setShowing((curr) => !curr);
+
+    console.log("I run all the time");
+
+    useEffect(() => {
+        console.log("I run only once");
+    }, []);
+
+    useEffect(() => {
+        if (keyword !== "") {
+            console.log("I run when 'keyword' changes which is", keyword);
+        }
+    }, [keyword]);
+
+    useEffect(() => {
+        if (counter !== 0) {
+            console.log("I run when 'counter' changes which is", counter);
+        }
+    }, [counter]);
+
+    useEffect(() => {
+        if (counter !== 0) {
+            console.log("I run when 'keyword' or 'counter' changes which is", counter);
+        }
+    }, [keyword, counter]);
+
+    return (
+        <div>
+            <div id="menu-bar">
+                <a href="/">Home</a>
+                <span> | </span>
+                <a href="/app-default">App Default</a>
+                <span> | </span>
+                <a href="/app-todo">App Todo</a>
+                <span> | </span>
+                <a href="/app-coins">App Coins</a>
+            </div>
+            <h1>App Default</h1>
+            <input
+                type="text"
+                placeholder="Search here..."
+                value={keyword}
+                onChange={onChange}
+            />
+            <h1 className={styles.title}>Welcome Back!</h1>
+            <h1 className={styles.title}>{counter}</h1>
+            <Button onClick={onClickCount} text={"Count up"} />
+            <button onClick={onClickShowing}>{showing ? "Hide" : "Show"}</button>
+            {showing ? <ShowData /> : null}
+        </div>
+    );
+}
+
+export default AppDefault;
