634891d71062f71ba10d47ab59cda58e9fedec3e
DongWon Sehr <dongwon@its-newid.com>
2022-08-14 20:54:18 +0900

    + menu bar

--- src/AppCoins.js
+++ src/AppCoins.js
@@ -121,6 +121,15 @@ function AppCoins() {
 
     return (
         <div>
+            <div id="menu-bar">
+                <a href="/">Home</a>
+                <span> | </span>
+                <a href="/app-default">App Default</a>
+                <span> | </span>
+                <a href="/app-todo">App Todo</a>
+                <span> | </span>
+                <a href="/app-coins">App Coins</a>
+            </div>
             <h1>
                 The Coins! ({coins.length}) <button onClick={onClickRefreshData}>Refresh Data</button>
             </h1>
--- src/AppTodo.js
+++ src/AppTodo.js
@@ -18,6 +18,15 @@ function AppTodo() {
 
     return (
         <div>
+            <div id="menu-bar">
+                <a href="/">Home</a>
+                <span> | </span>
+                <a href="/app-default">App Default</a>
+                <span> | </span>
+                <a href="/app-todo">App Todo</a>
+                <span> | </span>
+                <a href="/app-coins">App Coins</a>
+            </div>
             <h1>Todo</h1>
             <form onSubmit={onSubmit}>
                 <input
