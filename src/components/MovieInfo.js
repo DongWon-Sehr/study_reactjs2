7bcb5c4da1ad8aed9d33daa05f378fa35cd081cc
DongWon Sehr <dongwon@its-newid.com>
2022-08-28 13:30:51 +0900

    implement movie detail page

---
+++ src/components/MovieInfo.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function MovieInfo({ details }) {
+    console.log(details)
+    return (
+        <div>
+            <p id={details.id} key={details.id}>
+                <img src={details.medium_cover_image} alt='poster' />
+                <br />
+                <span className='title'>{details.title_long}</span>
+                <br />
+                <span className='basic-info'>{`🕒 ${details.runtime} mins  |  ⭐ ${details.rating}`}</span>
+                <br />
+                <span className='genre-info'>{`🔑 ${details.genres.map(genre => "#" + genre).join(" ")}`}</span>
+                <br />
+                <span className='summary-info'>{`📝 ${details.summary}`}</span>
+                <br />
+                <a href={details.url} target='_blank'>yts link</a>
+                <br />
+                <br />
+                <br />
+            </p>
+        </div>
+    );
+}
+
+MovieInfo.propTypes = {
+    details: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default MovieInfo;
--- src/routes/MovieDetails.js
+++ src/routes/MovieDetails.js
@@ -1,18 +1,23 @@
 import MenuBar from "../components/MenuBar";
+import MovieInfo from "../components/MovieInfo";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function MovieDetails() {
     const { id } = useParams();
     const movieDetailsApiUrl = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
-
+    
+    const [isLoading, setIsLoading] = useState(true);
     const [movieDetails, setMovieDetails] = useState({});
     const getMovieDetails = async () => {
+        setIsLoading(true);
         const response = await fetch(movieDetailsApiUrl);
         const json = await response.json();
         setMovieDetails(json.data.movie);
+        setIsLoading(false);
         console.log(json.data.movie);
     };
+
     useEffect(
         () => {
             getMovieDetails();
@@ -23,9 +28,12 @@ function MovieDetails() {
     return (
         <div>
             <MenuBar />
-            <h2>movie deatils!</h2>
-            <span>movie id: {id}</span>
-            <br />
+            <h1>
+                Details!
+            </h1>
+            {isLoading
+                ? <h3>Loading ...</h3>
+                : <MovieInfo details={movieDetails} />}
         </div>
     );
 }
